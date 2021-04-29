e07a28b990e8620c49ca92b0de181fc66ac4a860
shovity <shovity@hub_development_1>
2021-04-29 09:21:04 +0000

    add sed

--- eroc-init.sh
+++ eroc-init.sh
@@ -1,6 +1,11 @@
+__dirname=`dirname "$(readlink -f "$0")"`
+__secret=`openssl rand -base64 12`
+
 name=$1
 boot=${2-basic}
 
+cp -r $__dirname/boots/${boot} $name
+cd $name
 
-
-echo $name $boot
+find . -type f -exec sed -i "s/_app_name_/$name/g" {} \;
+find . -type f -exec sed -i "s/_secret_key_/$__secret/g" {} \;
